c8c001e0807708b158c6571ab10c715f57dcc6a8
LOctavio <luis_ram1997@hotmail.com>
2021-06-14 15:51:26 -0500

    Add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+    validates :title, :text, :image, presence:true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+    validates :name, :priority, presence: true
 end
