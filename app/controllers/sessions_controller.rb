22d60fbe9c7824ac31b110380dba33aa06a8de57
LOctavio <luis_ram1997@hotmail.com>
2021-06-15 12:43:14 -0500

    Add new and create methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,14 @@ class SessionsController < ApplicationController
     def new
         
     end
+
+    def create
+        @user = User.find_by(name: params[:name])
+        if @user
+           session[:user_id] = @user.id
+           redirect_to '/index'
+        else
+           redirect_to '/login'
+        end
+     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.create(params.require(:user).permit(:name))
+        session[:user_id] = @user.id
+        redirect_to '/index'
+     end
 end
