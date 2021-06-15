9a13abb3bb2414fba1ab7ecb896e0b92c51fe777
LOctavio <luis_ram1997@hotmail.com>
2021-06-15 13:00:44 -0500

    Add an if to check if a user was created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,18 @@ class UsersController < ApplicationController
     end
     
     def create
-        @user = User.create(params.require(:user).permit(:name))
-        session[:user_id] = @user.id
-        redirect_to '/index'
-     end
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to '/index'
+        else
+            redirect_to '/users/new'
+        end
+    end
+
+    private
+         
+    def user_params
+        params.require(:user).permit(:name)
+    end
 end
